399e94e13f5a4d6eeab185eb073c7e8aa56aa064
Aleksandra Danilova <alex.l.univ@gmail.com>
2020-04-16 22:12:26 +0300

    first

---
+++ shop/settings/base.py
@@ -0,0 +1,8 @@
+from datetime import date, datetime
+
+TITLE = 'GREENBAY'
+EMAIL = 'blabla@bla.com'
+ADDRESS = '123 Somewhere Over The Rainbow 67'
+TEXT = 'Eat your damn veggies'
+DATE = f'{date.today().strftime("%e of %B, %Y")}'
+PHONE_NUMBER = '+7 (812) 703-02-02'
---
+++ shop/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import *
+
+
+
+urlpatterns = [
+    path('', IndexView.as_view()),
+    path('shop/', ShopView.as_view(), name='shop'),
+    path('shop/<int:page_id>', ShopView.as_view()),
+    path('wishlist/', WishlistView.as_view(), name='wishlist'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('blog/', BlogView.as_view(), name='blog'),
+    path('blog-single', BlogSingleView.as_view(), name='blog'),
+    path('cart/', CartView.as_view(), name='cart'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('product-sinlge/', ProductSingleView.as_view(), name='product-single'),
+]
