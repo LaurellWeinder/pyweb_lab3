18a57e16c338bf14e12e9ccb2eec34d70be8e18b
Aleksandra Danilova <alex.l.univ@gmail.com>
2020-04-20 21:18:10 +0300

    Product model fixed

--- shop/models.py
+++ shop/models.py
@@ -7,15 +7,15 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    @staticmethod
-    def set_discount(discount) -> Decimal:
-        if discount > 0:
-            Product.price_sale = Product.price - Product.price / 100 * discount
-        return Product.price_sale
+
+    @property
+    def set_price_sale(self):
+        return self.price - self.price / 100 * self.discount
 
     name = models.CharField(max_length=50, primary_key=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    discount = models.DecimalField(max_digits=2, decimal_places=0, default=0)
+    discount = models.PositiveIntegerField(default=0)
     image = models.CharField(max_length=255)
-    price_sale = models.DecimalField(max_digits=10, decimal_places=2, default=set_discount.__get__(object))
 
+    class Meta:
+        ordering = ['-name'] # This is to fix pagination exception.
